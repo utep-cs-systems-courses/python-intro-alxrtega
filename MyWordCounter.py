93426f856bf81e438184468ee39c0c5177cab6d0
alejandro <a.ortega6243@gmail.com>
2020-08-31 08:55:08 -0600

    formatted words are being stored in the dictionary

--- MyWordCounter.py
+++ MyWordCounter.py
@@ -13,4 +13,23 @@ class MyWordCounter():
         print(self.inputFile)
         print(self.outputFile)
 
+    def printWords(self):
+        #each line will be formatted and stored into the dictionary
+        file = open(self.inputFile, "r")
+        for line in file:
+            line = line.strip()
+            line = line.lower()
+            wordsInLine = line.split(" ")
+
+            for word in wordsInLine:
+                #strip all non-alpha characters from the word
+                word = re.sub('[^A-Za-z0-9]+', '', word)
+
+                #check if word is in dictionary, if not then add it
+                if word in self.wordCounter:
+                    self.wordCounter[word] += 1
+                else:
+                    self.wordCounter[word] = 1
+
 tester = MyWordCounter()
+tester.printWords()
